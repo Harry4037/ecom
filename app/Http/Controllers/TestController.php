4dd336d960796931cddb2a6e06ad9a6eae263b5e
Hariom <hariom@gmail.com>
2019-10-23 22:27:15 +0530

    rc

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
+use App\Car;
+
+class TestController extends Controller {
+
+    public function test() {
+        $car = new Car();
+        $car->carcompany = 'abc';
+        $car->model = 'xyz';
+        $car->price = 100;
+        $car->save();
+        $cars = DB::collection('cars')->get();
+        dd($cars);
+    }
+
+}
